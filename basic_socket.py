775ae466c68063e112b606a5e258921ad287a948
omarf14 <omarf@fossa.systems>
2025-10-16 16:07:23 +0200

    feat: encryption and crc check

--- basic_socket.py
+++ basic_socket.py
@@ -1,18 +1,7 @@
 import socket
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(("0.0.0.0", 52001))
-
-
-import os
-import struct
-import hashlib
-import hmac
-import ctypes
-import codecs
-import time
 from fec import PacketHandler
-# from bpsk_decoder_fec import decoder_queue
+from crypto import decrypt_aes
+from zlib import crc32
 
 
 VITERBI_RATE = 2
@@ -37,46 +26,35 @@ ACCES_KEY_CONVOLVED_64B = 0xB9F8B220B1CF12BC
 THRESHOLD64 = 50
 THRESHOLD32 = 26
 
-path = os.path.dirname(os.path.abspath(__file__))
-bbfec = ctypes.CDLL(path + "/bbfec.so")
-# viterbi
-bbfec.create_viterbi.argtypes = [ctypes.c_int16]
-bbfec.create_viterbi.restype = ctypes.c_void_p
-bbfec.init_viterbi.argtypes = [ctypes.c_void_p, ctypes.c_int]
-bbfec.init_viterbi.restype = ctypes.c_int
-bbfec.update_viterbi.argtypes = [ctypes.c_void_p, ctypes.c_char_p, ctypes.c_uint16]
-bbfec.update_viterbi.restype = ctypes.c_int
-bbfec.chainback_viterbi.argtypes = [ctypes.c_void_p, ctypes.c_char_p, ctypes.c_uint, ctypes.c_uint]
-bbfec.chainback_viterbi.restype = ctypes.c_int
-bbfec.delete_viterbi.argtypes = [ctypes.c_void_p]
-bbfec.delete_viterbi.restype = None
-bbfec.encode_viterbi.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_int]
-bbfec.encode_viterbi.restype = None
-# rs
-bbfec.encode_rs.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_int]
-bbfec.encode_rs.restype = None
-bbfec.decode_rs.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_int, ctypes.c_int]
-bbfec.decode_rs.restype = ctypes.c_int
-# randomizer
-bbfec.ccsds_generate_sequence.argtypes = [ctypes.c_char_p, ctypes.c_int]
-bbfec.ccsds_generate_sequence.restype = None
-bbfec.ccsds_xor_sequence.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_int]
-bbfec.ccsds_xor_sequence.restype = None
+ASM_SIZE    = 4
+
+# Decoding payload indexes
+PAYLOAD_LEN_IDX             = 0
+PAYLOAD_LEN_SIZE            = 2
+CSP_HEADER_IDX              = PAYLOAD_LEN_SIZE
+CSP_HEADER_SIZE             = 4
+CONTROL_HEADER_IDX          = CSP_HEADER_IDX+CSP_HEADER_SIZE
+CONTROL_HEADER_SIZE         = 5
+PAYLOAD_DATA_IDX            = CONTROL_HEADER_IDX+CONTROL_HEADER_SIZE
+PAYLOAD_DATA_SIZE           = 208
+CRC_IDX                     = PAYLOAD_DATA_IDX+PAYLOAD_DATA_SIZE
+CRC_SIZE                    = 4
+TOTAL_PAYLOAD_SIZE          = CRC_IDX+CRC_SIZE
+
+
+aes_key = "1fcbf1aa4e41f18c08524712541e432a"
 
 def decoder(message):
     ec = PacketHandler(None)
     print(f"INFO: Decoding message len {len(message)}")
     
     byte_array = bytes((sum(message[k * 8 + j] << (7 - j) for j in range(8)) for k in range(TOTAL_FRAME_BYTE_LEN)))
-    # byte_array = bytes((
-    #     sum(message[k * 8 + j] << j for j in range(8))  # LSB first
-    #     for k in range(TOTAL_FRAME_BYTE_LEN)
-    #     ))
 
 
     ##### DEBUGGING PRINTING #####
     # print("Original data:\n{0}\n".format(ec.hexdump(byte_array)))
     ##############################
+
 
     try:
         # Unconvolve data to check ASM
@@ -91,7 +69,7 @@ def decoder(message):
 
 
         # Check Attached Sync Marker after viterbi processing
-        found_asm = int.from_bytes(data[0:4], byteorder='little')
+        found_asm = int.from_bytes(data[:ASM_SIZE], byteorder='little')
         diff = found_asm ^ ACCESS_KEY_32B
         matches32 = 32 - bin(diff).count('1')
 
@@ -101,15 +79,20 @@ def decoder(message):
 
         if matches32 >= THRESHOLD32:
             # Decode payload 
-            payload, bit_corr, byte_corr = ec.decode_fec(data[4:258])
-            payload_len = int.from_bytes(payload[0:2], byteorder='little')
-            # print(f"INFO: Acces key found at index {idx}, matches = {matches64}, matches2 = {matches32}")
-            # payload_len = 255 if payload_len > 255 else payload_len
-            print("INFO: Decode success with {} corrected bits, payload_len {} payload: \n{}\n".
-                    format(bit_corr, payload_len, payload[2:payload_len].decode('utf-8', errors='replace')))
-            # msg_ctr += 1
-            # i = i + TOTAL_FRAME_BIT_LEN - 8*10
-            # continue
+            payload, bit_corr, byte_corr = ec.decode_fec(data[ASM_SIZE:ASM_SIZE+RS_BLOCK_LENGTH])
+            # payload_len = int.from_bytes(payload[:PAYLOAD_LEN_SIZE], byteorder='little')
+            # print("INFO: Decode success with {} corrected bits, payload_len {} payload: \n{}\n".
+            #         format(bit_corr, payload_len, payload[2:payload_len].decode('utf-8', errors='replace')))
+            print("Decoded data: \n{0}\n".format(ec.hexdump(payload[PAYLOAD_LEN_SIZE:TOTAL_PAYLOAD_SIZE])))
+            payload_crc = crc32(payload[CSP_HEADER_IDX:CRC_IDX]) & 0xFFFFFFFF
+            received_crc = int.from_bytes(payload[CRC_IDX:TOTAL_PAYLOAD_SIZE], byteorder='little')  # or 'little'
+            if payload_crc != received_crc:
+                print("ERROR: payload CRC missmatch")
+            decrypted = decrypt_aes(payload[PAYLOAD_DATA_IDX:CRC_IDX], aes_key)
+            print("Decrypted payload data: \n{0}\n".format(ec.hexdump(decrypted)))
+        else:
+            print(f"ERROR: Matches below threshold {matches32}")
+
 
     except Exception as e:
 
@@ -119,10 +102,14 @@ def decoder(message):
         ##############################
     
 
-while True:
-    data, addr = sock.recvfrom(4144)
+def main():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(("0.0.0.0", 52001))
 
-    # print(f"\n🔔 PDU ready: {len(data)} bytes")
-    # print(list(data[:20]), "...")
-    # print("Received:", data)
-    decoder(data)
+    while 1:
+        data, addr = sock.recvfrom(4144)
+        decoder(data)
+
+
+if __name__ == '__main__':
+    main()
