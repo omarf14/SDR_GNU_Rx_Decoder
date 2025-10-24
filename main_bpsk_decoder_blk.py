38ee5c7650b03d9bf8f9b4b8c7faa58a733cc540
omarf14 <omarf@fossa.systems>
2025-10-24 12:29:56 +0200

    refactor: defines and source selector

--- main_bpsk_decoder_blk.py
+++ main_bpsk_decoder_blk.py
@@ -33,6 +33,34 @@ from tag_printer_blk import tag_printer_bb
 from tag_to_pdu_blk import tag_to_pdu_udp_bb
 
 
+# Rx source config
+SOURCE_PLUTO = 0
+SOURCE_USRP = 1
+SOURCE = SOURCE_PLUTO
+
+
+# Rx sync config
+LO_FREQ = 2.23e9
+SAMPLE_RATE = 1000000
+BW = 250000
+SPS = 8
+TED_GAIN = 0.02
+ASM_bin = '1011100111111000101100100010000010110001110011110001001010111100'
+ASM_thr = 8
+# To tune
+RF_GAIN = 45
+SYNC_LOOP = 0.02
+COSTAS_LOOP = 0.025
+if SOURCE == SOURCE_USRP:
+    RF_GAIN = 30
+    SYNC_LOOP = 0.008
+    COSTAS_LOOP = 0.01
+
+
+# PDU and UDP client config
+PDU_LEN = 4144  #(RS_BLOCK_LENGTH + RAW_ASM_BYTE_LEN)*VITERBI_RATE*8
+UDP_IP = '127.0.0.1'
+UDP_PORT = 52001
 
 
 class pluto(gr.top_block, Qt.QWidget):
@@ -71,35 +99,35 @@ class pluto(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.bw = bw = 250000
+        self.bw = bw = BW
         self.variable_tag_object_0 = variable_tag_object_0 = gr.tag_utils.python_to_tag((0, pmt.intern("key"), pmt.intern("value"), pmt.intern("src")))
-        self.sync_loop = sync_loop = 0.008
-        self.samp_rate = samp_rate = 1000000
-        self.rf_gain = rf_gain = 45
-        self.freq = freq = 2.23e9
-        self.costas_loop = costas_loop = 0.01
+        self.sync_loop = sync_loop = SYNC_LOOP
+        self.samp_rate = samp_rate = SAMPLE_RATE
+        self.rf_gain = rf_gain = RF_GAIN
+        self.freq = freq = LO_FREQ
+        self.costas_loop = costas_loop = COSTAS_LOOP
         self.bwidth = bwidth = bw
         self.bpsk_o_2 = bpsk_o_2 = digital.constellation_calcdist([-1-0j, +1+0j], [0, 1],
         2, 1, digital.constellation.AMPLITUDE_NORMALIZATION).base()
         # self.bpsk_o_2.set_npwr(1.0)
         self.bpsk_o = bpsk_o = digital.constellation_bpsk().base()
         # self.bpsk_o.set_npwr(1.0)
-        self.TED_gain = TED_gain = 0.02
+        self.TED_gain = TED_gain = TED_GAIN
 
         ##################################################
         # Blocks
         ##################################################
 
-        self._sync_loop_range = qtgui.Range(0.001, 0.05, 0.001, 0.008, 200)
+        self._sync_loop_range = qtgui.Range(0.001, 0.05, 0.001, sync_loop, 200)
         self._sync_loop_win = qtgui.RangeWidget(self._sync_loop_range, self.set_sync_loop, "'sync_loop'", "counter_slider", float, QtCore.Qt.Horizontal)
         self.top_layout.addWidget(self._sync_loop_win)
-        self._rf_gain_range = qtgui.Range(0, 150, 10, 35, 200)
+        self._rf_gain_range = qtgui.Range(0, 150, 10, rf_gain, 200)
         self._rf_gain_win = qtgui.RangeWidget(self._rf_gain_range, self.set_rf_gain, "'rf_gain'", "counter_slider", float, QtCore.Qt.Horizontal)
         self.top_layout.addWidget(self._rf_gain_win)
-        self._freq_range = qtgui.Range(2e9, 2.5e9, 2e3, 2.23e9, 200)
+        self._freq_range = qtgui.Range(2e9, 2.5e9, 2e3, freq, 200)
         self._freq_win = qtgui.RangeWidget(self._freq_range, self.set_freq, "'freq'", "counter_slider", float, QtCore.Qt.Horizontal)
         self.top_layout.addWidget(self._freq_win)
-        self._costas_loop_range = qtgui.Range(0, 0.1, 0.01, 0.01, 200)
+        self._costas_loop_range = qtgui.Range(0, 0.1, 0.01, costas_loop, 200)
         self._costas_loop_win = qtgui.RangeWidget(self._costas_loop_range, self.set_costas_loop, "'costas_loop'", "counter_slider", float, QtCore.Qt.Horizontal)
         self.top_layout.addWidget(self._costas_loop_win)
         self._bwidth_range = qtgui.Range(200e3, 10e6, 10000, bw, 200)
@@ -232,19 +260,39 @@ class pluto(gr.top_block, Qt.QWidget):
                 50000,
                 window.WIN_HAMMING,
                 0.35))
-        self.iio_pluto_source_0 = iio.fmcomms2_source_fc32('' if '' else iio.get_pluto_uri(), [True, True], 32768)
-        self.iio_pluto_source_0.set_len_tag_key('packet_len')
-        self.iio_pluto_source_0.set_frequency(2230000000)
-        self.iio_pluto_source_0.set_samplerate(1000000)
-        self.iio_pluto_source_0.set_gain_mode(0, 'manual')
-        self.iio_pluto_source_0.set_gain(0, rf_gain)
-        self.iio_pluto_source_0.set_quadrature(True)
-        self.iio_pluto_source_0.set_rfdc(True)
-        self.iio_pluto_source_0.set_bbdc(True)
-        self.iio_pluto_source_0.set_filter_params('Auto', '', 0, 0)
+
+
+        if SOURCE == SOURCE_USRP:
+            self.source = uhd.usrp_source(
+                ",".join(("", '')),
+                uhd.stream_args(
+                    cpu_format="sc16",
+                    args='',
+                    channels=list(range(0,1)),
+                ),
+            )
+            self.source.set_samp_rate(samp_rate)
+            self.source.set_center_freq(freq, 0)
+            self.source.set_antenna("RX2", 0)
+            self.source.set_bandwidth(bw, 0)
+            self.source.set_gain(rf_gain, 0)
+            self.source.set_auto_dc_offset(True, 0)
+            self.source.set_auto_iq_balance(True, 0)
+        else:
+            self.source = iio.fmcomms2_source_fc32('' if '' else iio.get_pluto_uri(), [True, True], 32768)
+            self.source.set_len_tag_key('packet_len')
+            self.source.set_frequency(LO_FREQ)
+            self.source.set_samplerate(SAMPLE_RATE)
+            self.source.set_gain_mode(0, 'manual')
+            self.source.set_gain(0, rf_gain)
+            self.source.set_quadrature(True)
+            self.source.set_rfdc(True)
+            self.source.set_bbdc(True)
+            self.source.set_filter_params('Auto', '', 0, 0)
+
         self.digital_symbol_sync_xx_0_0 = digital.symbol_sync_cc(
             digital.TED_MUELLER_AND_MULLER,
-            8,
+            SPS,
             sync_loop,
             1.0,
             0.1,
@@ -256,7 +304,7 @@ class pluto(gr.top_block, Qt.QWidget):
             [])
         self.digital_diff_decoder_bb_0 = digital.diff_decoder_bb(2, digital.DIFF_DIFFERENTIAL)
         self.digital_costas_loop_cc_0_0 = digital.costas_loop_cc(costas_loop, 2, False)
-        self.digital_correlate_access_code_tag_xx_0 = digital.correlate_access_code_tag_bb('1011100111111000101100100010000010110001110011110001001010111100', 8, 'ac_found')
+        self.digital_correlate_access_code_tag_xx_0 = digital.correlate_access_code_tag_bb(ASM_bin, ASM_thr, 'ac_found')
         self.digital_binary_slicer_fb_0 = digital.binary_slicer_fb()
         self.blocks_correctiq_auto_0_0_0 = blocks.correctiq_auto(samp_rate, freq, 1.5, 2)
         self.blocks_complex_to_float_0 = blocks.complex_to_float(1)
@@ -269,10 +317,10 @@ class pluto(gr.top_block, Qt.QWidget):
         # self.tag_to_pdu = tag_to_pdu_udp_bb('ac_found', 4144, '127.0.0.1', 52001)
         self.tag_to_pdu = tag_to_pdu_udp_bb(
             tag_key='ac_found',
-            pdu_len=4144,
+            pdu_len=PDU_LEN,
             # marker_len=64,
-            udp_ip='127.0.0.1',
-            udp_port=52001
+            udp_ip=UDP_IP,
+            udp_port=UDP_PORT
         )
 
 
@@ -282,7 +330,7 @@ class pluto(gr.top_block, Qt.QWidget):
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.iio_pluto_source_0, 0), (self.low_pass_filter_0, 0))
+        self.connect((self.source, 0), (self.low_pass_filter_0, 0))
         # self.connect((self.low_pass_filter_0, 0), (self.analog_pwr_squelch_xx_0, 0))
         self.connect((self.low_pass_filter_0, 0), (self.qtgui_freq_sink_x_0, 0))
         self.connect((self.low_pass_filter_0, 0), (self.qtgui_waterfall_sink_x_0, 0))
